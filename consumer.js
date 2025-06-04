eb0c9c1ce970ef8bb3c7c8cdeb50620f8fbf5b45
Ritushreedas-Infomaticae <ritushree.das@infomaticae.co.in>
2025-06-04 18:17:25 +0530

    hello

--- consumer.js
+++ consumer.js
@@ -5,77 +5,91 @@ let client;
 let pubSubClient;
 
 try {
-    client = createClient();
-    await client.connect();
-    client.on('error', (err) => {
-        console.error('❌ Redis client error:', err);
-    });
+  client = createClient();
+  await client.connect();
+  client.on("error", (err) => {
+    console.error("❌ Redis client error:", err);
+  });
 
-    pubSubClient = createClient();
-    await pubSubClient.connect();
-    pubSubClient.on('error', (err) => {
-        console.error('❌ Redis pubsub client error:', err.message);
-    });
+  pubSubClient = createClient();
+  await pubSubClient.connect();
+  pubSubClient.on("error", (err) => {
+    console.error("❌ Redis pubsub client error:", err.message);
+  });
 
-
-    console.log("✅ Successfully connected to redis");
+  console.log("✅ Successfully connected to redis");
 } catch (error) {
-    console.log("❌ Error connecting to redis", error)
+  console.log("❌ Error connecting to redis", error);
 }
-
 
 const QUEUE = "ride-request-queue";
 
 const channel = await connectRabbitMQ(QUEUE);
 
 channel.consume(QUEUE, async (msg) => {
-    if (msg !== null) {
-        try {
-            const { rideId, src, dest, passengerId } = JSON.parse(msg.content.toString());
-            console.log(`ride request src:${JSON.stringify(src)} dest:${JSON.stringify(dest)} received`);
+  if (msg !== null) {
+    try {
+      const { rideId, src, dest, passengerId, price, vehicleType } = JSON.parse(
+        msg.content.toString()
+      );
+      console.log(
+        `ride request src:${JSON.stringify(src)} dest:${JSON.stringify(
+          dest
+        )} received price:${JSON.stringify(price)}`
+      );
 
-            const results = await client.geoSearchWith('active_drivers',
-                { latitude: Number(src.lat), longitude: Number(src.lng) },
-                { radius: 50, unit: 'km' },
-                ['WITHDIST', 'WITHCOORD'],
-                { SORT: 'ASC', COUNT: 5 });
+      const results = await client.geoSearchWith(
+        "active_drivers",
+        { latitude: Number(src.lat), longitude: Number(src.lng) },
+        { radius: 50, unit: "km" },
+        ["WITHDIST", "WITHCOORD"],
+        { SORT: "ASC", COUNT: 5 }
+      );
 
+      if (results.length === 0) {
+        console.log("❌ No drivers available nearby.");
+        channel.nack(msg); // requeue = true by default
+        console.log(
+          `requeued ride request with src:${JSON.stringify(
+            src
+          )} dest:${JSON.stringify(dest)}`
+        );
+      } else {
+        console.log("🔎 Nearby drivers found:");
 
-            if (results.length === 0) {
-                console.log("❌ No drivers available nearby.");
-                channel.nack(msg); // requeue = true by default
-                console.log(`requeued ride request with src:${JSON.stringify(src)} dest:${JSON.stringify(dest)}`)
-            } else {
-                console.log("🔎 Nearby drivers found:");
+        results.forEach(async ({ member: driverId, distance }) => {
+          console.log(`- ${driverId} (${Number(distance).toFixed(2)} km away)`);
 
-                results.forEach(async ({ member: driverId, distance }) => {
-                    console.log(`- ${driverId} (${Number(distance).toFixed(2)} km away)`);
+          await pubSubClient.publish(
+            "ride-requests",
+            JSON.stringify({
+              rideId,
+              driverId,
+              passengerId,
+              src,
+              dest,
+              vehicleType,
+              price,
+              distance: Number(distance).toFixed(2),
+            })
+          );
 
-                    await pubSubClient.publish('ride-requests', JSON.stringify({
-                        rideId,
-                        driverId,
-                        passengerId,
-                        src,
-                        dest,
-                        distance: Number(distance).toFixed(2),
-                    }));
+          console.log(
+            `📨 Published ride request ${rideId} for driver ${driverId}`
+          );
+        });
+        channel.ack(msg);
+      }
+    } catch (error) {
+      console.error("⚠️ Error while processing ride request:", error);
 
-                    console.log(`📨 Published ride request ${rideId} for driver ${driverId}`);
-                }
-                )
-                channel.ack(msg);
-            }
-        } catch (error) {
-            console.error('⚠️ Error while processing ride request:', error);
-
-            if (error.message.includes('invalid longitude,latitude pair')) {
-                console.log('⚠️ Bad coordinates received, discarding message.');
-                channel.ack(msg);
-            } else {
-                console.log('⚠️ Unexpected error, requeueing message...');
-                channel.nack(msg, false, true); // requeue = true
-            }
-        }
-
+      if (error.message.includes("invalid longitude,latitude pair")) {
+        console.log("⚠️ Bad coordinates received, discarding message.");
+        channel.ack(msg);
+      } else {
+        console.log("⚠️ Unexpected error, requeueing message...");
+        channel.nack(msg, false, true); // requeue = true
+      }
     }
-})
+  }
+});
